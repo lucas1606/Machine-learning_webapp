d9530d309b2c6071054d1bf7b5d912904b91a14f
lucas1606 <lucas.almeida1606@gmail.com>
2021-09-06 19:54:52 -0300

    Created methods for encoding categorical features and for replacing na values

--- package/classes.py
+++ package/classes.py
@@ -1,21 +1,14 @@
 import pandas as pd
 import numpy as np
-import sys
+from sklearn.preprocessing import LabelEncoder
 
-class Data:
+
+
+class Helpers:
     '''
     Recives a csv relative path and extract ist propeties in its inicial estate
     Parameters
     '''
-    def __init__(self, path, sep=None):
-        self._path = path
-        self._sep = ','
-        self._df = pd.read_csv(self._path, sep=self._sep)
-        
-    @property
-    def df(self):
-        return self._df
-
     def __repr__(self):
             return repr(self.df)
             
@@ -38,21 +31,51 @@ class Data:
     def na_columns(self):
         return list(self.df.loc[:,self.df.isna().sum() > 0].columns)
 
-class NaSolver(Data): #Only basic functions
+class NaSolver(Helpers): #Only basic functions
     def __init__(self, df:object):
-        self._df = df
+        self.df = df
 
-    def remove_na_lines(self, na_proportion=None):
-        print(f'Lines removed:{self.na_lines[0:4]}...')
-        self._df = self.df.drop(self.na_lines)
-        return self._df
+    def remove_na_lines(self, max_na_proportion=None):
+        if max_na_proportion:
+            drop_lines = list(self.df.loc[self.df.iloc[self.na_lines].isna().sum(axis=1) > len(self.df.columns)*max_na_proportion].index)
+        else:
+            drop_lines = self.na_lines
+        print(f'Lines removed:{drop_lines[0:4]}...')
+        self.df = self.df.drop(drop_lines)
+        return self.df
 
-    def remove_na_columns(self, na_proportion=None):
-        print(f'Columns removed')
-        self._df = self.df.drop(columns=self.na_columns)
-        return self._df
+    def remove_na_columns(self, max_na_proportion=None):
+        if max_na_proportion:
+            drop_columns = list(self.df.loc[:,self.df.isnull().sum() > len(self.df.index)*max_na_proportion].columns)
+        print(f'Columns removed{drop_columns[0:4]}...')
+        self.df = self.df.drop(columns=drop_columns)
+        return self.df
 
-    
+    def replace_na_values(self):
+        for column in self.na_columns:
+            if self.df[column].dtype == 'object':
+                self.df[column].fillna(self.df[column].mode(), inplace = True) 
+
+            if self.df[column].dtype == 'int64':
+                self.df[column].fillna(self.df[column].median(), inplace = True)
+
+            if self.df[column].dtype == 'float64':
+                self.df[column].fillna(self.df[column].median(), inplace = True)
+        return self.df
+
+    def encode_categorical(self):
+        label_maker = LabelEncoder()
+        columns = list(self.df.loc[:,(self.df.dtypes == 'object')].columns)
+        for col in columns:
+            self.df[col] = label_maker.fit_transform(self.df[col].astype('str'))
+        return self.df
+
+
+
+
+       
+
+
     
        
         
@@ -60,5 +83,8 @@ class NaSolver(Data): #Only basic functions
         
 
 
-#sys.path.append(".")
+
+
+
+
 
